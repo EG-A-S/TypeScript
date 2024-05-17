de17cf17ce41150f7bf86a2248bed824e800e813
snovader <marcus.ording@hotmail.com>
2024-05-17 13:03:40 +0200

    Reduce tsserver size

--- src/compiler/moduleSpecifiers.ts
+++ src/compiler/moduleSpecifiers.ts
@@ -126,7 +126,7 @@ import {
     TypeChecker,
     UserPreferences,
 } from "./_namespaces/ts.js";
-import { getPnpApi } from "./pnp.js";
+import { getPnpApi } from "./pnpapi.js";
 
 // Used by importFixes, getEditsForFileRename, and declaration emit to synthesize import module specifiers.
 
