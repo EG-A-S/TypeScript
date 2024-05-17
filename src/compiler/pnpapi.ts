e4ee283961db1c1d1bf78d5a34f86654581a2bd4
snovader <marcus.ording@hotmail.com>
2024-05-17 13:05:08 +0200

    Hoist require of `module` for perf

--- src/compiler/pnpapi.ts
+++ src/compiler/pnpapi.ts
@@ -1,12 +1,13 @@
 // To preserve the effects of https://github.com/microsoft/TypeScript/pull/55326
 // this file needs to avoid importing large graphs.
 
+const { findPnpApi } = require("module");
+
 export function getPnpApi(path: string) {
     if (typeof process.versions.pnp === "undefined") {
         return;
     }
 
-    const { findPnpApi } = require("module");
     if (findPnpApi) {
         return findPnpApi(`${path}/`);
     }
